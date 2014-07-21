bf0c7116f0cb4a43a4f95b04d943ef81d60a8a84
Almad <bugs@almad.net>
2014-07-21 22:36:16 +0200

    Do not override install_requires

--- pavement.py
+++ pavement.py
@@ -35,7 +35,6 @@ setup(
 if paver.setuputils.has_setuptools:
     old_sdist = "setuptools.command.sdist"
     options.setup.update(dict(
-        install_requires=[],
         test_suite='nose.collector',
         zip_safe=False,
         entry_points="""
