5327f08e526170568c0d92d173f6b6175baa9ef7
Almad <bugs@almad.net>
2014-09-08 15:40:24 +0200

    Version bump to 0.8.1

--- pavement.py
+++ pavement.py
@@ -4,7 +4,7 @@ from paver.setuputils import setup
 options = environment.options
 
 NAME = 'blackbelt'
-VERSION = '0.8.0'
+VERSION = '0.8.1'
 
 requires = ['click', 'requests', 'trello', 'PyGithub']
 
