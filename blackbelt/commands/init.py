bcee7fcca784bbc55fc7d10f9dbfeaaf3603c42a
Ladislav Prskavec <ladislav@prskavec.net>
2015-01-05 16:12:46 +0100

    Revert changes message in init help

--- blackbelt/commands/init.py
+++ blackbelt/commands/init.py
@@ -5,7 +5,7 @@ import click
 from blackbelt.configure import configure_blackbelt
 
 
-@click.group(invoke_without_command=True, help="""Deploy to staging""")
+@click.group(invoke_without_command=True, help="""Initialize application for usage. Invoke this command first.""")
 def cli():
     configure_blackbelt()
 
