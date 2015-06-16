930d9710777026450650e2118bb83fe176b3f9ed
Ladislav Prskavec <ladislav@apiary.io>
2015-06-16 13:54:36 +0200

    Add missing room argument

--- blackbelt/messages.py
+++ blackbelt/messages.py
@@ -1,4 +1,4 @@
 from blackbelt.slack import post_message as slack_post_message
 
-def post_message(message):
-  slack_post_message(message)
+def post_message(message, room):
+  slack_post_message(message, room)
