89671b5d5dd9019c6571c051c26bc2cfb983f02f
Ladislav Prskavec <ladislav.prskavec@oracle.com>
2017-12-12 13:36:46 +0100

    Version bump to 0.15.2

--- pavement.py
+++ pavement.py
@@ -4,7 +4,7 @@ from paver.setuputils import setup
 options = environment.options
 
 NAME = 'blackbelt'
-VERSION = '0.15.1'
+VERSION = '0.15.2'
 
 requires = ['six', 'click', 'requests', 'PyGithub', 'slacker>=0.6.2', 'pyopenssl', 'ndg-httpsclient', 'pyasn1']
 
