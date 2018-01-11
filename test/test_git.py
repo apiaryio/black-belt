3a6aee49bb2690288f5c0b7a6c3a9a61c32aec99
miiila <mila.votradovec@gmail.com>
2018-01-11 15:48:57 +0100

    test: add test and adjust code to it for git branch output

--- test/test_git.py
+++ test/test_git.py
@@ -1,6 +1,7 @@
 from nose.tools import assert_equal, assert_raises
-
-from blackbelt.git import get_remote_repo_info
+from mock import patch
+from sys import version_info
+from blackbelt.git import get_remote_repo_info, get_current_branch
 
 
 class TestGithubRepoGitAddressParsing(object):
@@ -29,3 +30,26 @@ class TestGithubRepoHttpsAddressParsing(object):
 
     def test_repo_name(self):
         assert_equal('apiary-test', self.parsed['name'])
+
+
+class TestGitBranchOutput(object):
+
+    @patch('subprocess.check_output')
+    def test_branch_name_parsing(self, check_output_mock):
+        branch_name = 'prefix/branch-name'
+        if (version_info[0] == 3):
+            check_output_mock.return_value = bytes(branch_name, 'utf-8')
+        else:
+            check_output_mock.return_value = str(branch_name)
+
+        assert_equal(branch_name, get_current_branch())
+
+    @patch('subprocess.check_output')
+    def test_branch_name_parsing_with_trailing_whitespace(self, check_output_mock):
+        branch_name = 'prefix/branch-name'
+        if (version_info[0] == 3):
+            check_output_mock.return_value = bytes(branch_name+'\n', 'utf-8')
+        else:
+            check_output_mock.return_value = str(branch_name+'\n')
+
+        assert_equal(branch_name, get_current_branch())
